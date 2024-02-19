32e93d1fb4b601aabd1929fd089b41d2b8530fda
Lucas Cubile <cubilelucas@gmail.com>
2024-02-19 15:11:30 -0300

    Add last three projects to docs

--- js/book.mjs
+++ js/book.mjs
@@ -1,8 +1,13 @@
-export function Book(title, author, pages, isRead, position) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this['is-read'] = isRead ? 'Read' : 'Not read'
-  this.index = position
-  this.info = () => `${this.title} by ${this.author}, ${this.pages}, ${this['is-read']}`
+export class Book {
+  constructor(title, author, pages, isRead, position) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this['is-read'] = isRead ? 'Read' : 'Not read'
+    this.index = position
+  }
+
+  getinfo() {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this['is-read']}`
+  }
 }
